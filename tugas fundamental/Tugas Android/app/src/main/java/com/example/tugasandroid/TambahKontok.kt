bf9da63f3970af8544a511004901525617d6e2be
Steffany <steffanyfanyy2@gmail.com>
2023-10-07 12:30:57 +0700

    test

---
+++ tugas fundamental/Tugas Android/app/src/main/java/com/example/tugasandroid/TambahKontok.kt
@@ -0,0 +1,11 @@
+package com.example.tugasandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahKontok : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_kontok)
+    }
+}
